686a1bef68d83b8470ca147d4bbe713b73890bc3
Nikolas Silva <nikolas.rsilva@gmail.com>
2024-01-24 14:57:17 -0300

    chore: initial commit

---
+++ source/cli.tsx
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(`
+	Usage
+	  $ g666
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ g666 --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
